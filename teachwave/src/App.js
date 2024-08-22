5345e6a4774a3e371155630a6cf5e539b45ca37a
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-08-22 13:07:03 +0530

    update

--- teachwave/src/App.js
+++ teachwave/src/App.js
@@ -1,37 +1,23 @@
-import React, { useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import LoadingScreen from './components/Loading'; // Adjust the path as per your project structure
-
-// Placeholder MainContent component
-const MainContent = () => {
-  return (
-    <div>
-      <h1>Main Application Content</h1>
-      <p>Welcome to the main content of your application!</p>
-      {/* Add more components and features here */}
-    </div>
-  );
-};
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const isLoggedIn = true; // Simulating login status (replace with actual logic)
-
-  // Function to handle completion of loading (simulating an API call or async operation)
-  const handleLoadingComplete = () => {
-    setTimeout(() => {
-      setIsLoading(false); // Update state to reflect loading completion
-    }, 2000); // Simulating 2 seconds delay for loading
-  };
-
   return (
     <div className="App">
-      <Routes>
-        <Route 
-          path="/" 
-          element={isLoading ? <LoadingScreen onComplete={handleLoadingComplete} /> : isLoggedIn ? <MainContent /> : <Navigate to="/login" />} 
-        />
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
